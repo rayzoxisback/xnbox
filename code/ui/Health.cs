c3798855b4bad5fed921c572d6c346153300644c
Kévin HENSCHEN <elkhenschen@gmail.com>
2021-08-09 21:09:54 +0200

    [EDIT] : Hud Improved

--- code/ui/Health.cs
+++ code/ui/Health.cs
@@ -16,6 +16,6 @@ public class Health : Panel
 		var player = Local.Pawn;
 		if ( player == null ) return;
 
-		Label.Text = $"{player.Health.CeilToInt()}";
+		Label.Text = $"💖 {player.Health.CeilToInt()}";
 	}
 }
